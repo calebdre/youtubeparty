9eac2ca63b27cd7be349939d026f5a6aaa62adb6
Andrew McPherson <andrewmcp333@gmail.com>
2014-06-20 19:43:38 -0400

    Attempted to figure out more of the Youtube API.

--- youtubeparty.js
+++ youtubeparty.js
@@ -1,12 +1,23 @@
+var YoutubeQueue = new Meteor.Collection("queue");
+
 if (Meteor.isClient)
 {
-	Template.upstage.yturl = "https://www.youtube.com/v/7POSA7kFxGU?enablejsapi=1"; //&autoplay=1
+	/*Template.onstage.yturl = function()
+	{
+		var next_in_queue = YoutubeQueue.findOne({}, {sort: {timestamp: 1}});
+		var yturl = next_in_queue ? next_in_queue.yturl : "KyUYbAWjdx8";
+
+		return "https://www.youtube.com/v/" + yturl + "?version=3&enablejsapi=1"; //&autoplay=1
+	}*/
 }
 
 if(Meteor.isServer)
 {
 	Meteor.startup(function()
 	{
-		
+		YoutubeQueue.remove({});
+		YoutubeQueue.insert({yturl: "ab8TumnQDTY", timestamp: Date.now() + 10});
+		YoutubeQueue.insert({yturl: "0DcS1ofQXt8", timestamp: Date.now() + 20});
+		YoutubeQueue.insert({yturl: "QLbt6tAQOT8", timestamp: Date.now() + 30});
 	});
 }

c8267f1c0d367335b98dcde6c1155a1133d75a01
Andrew McPherson <andrewmcp333@gmail.com>
2014-07-14 22:39:38 -0400

    Connected the frontend and backend via websockets.

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require("express");
 
-var server = express();
-server.use(express.static("./resources"));
-
-server.get("/", function(request, response)
+application = express();
+application.use(express.static("./resources"));
+application.get("/", function(request, response)
 {
 	response.redirect("party.html");
 });
+server = http.Server(application);
+server.listen(process.env.PORT || 1271);
 
-server.listen(1271);
+var io = require("socket.io")(server);
+io.on("connection", function(socket)
+{
+	console.log("connected!");
+});

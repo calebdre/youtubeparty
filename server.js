255c931e9823347fc2a2b7434d6063c43e7ec02e
Andrew McPherson <andrewmcp333@gmail.com>
2014-07-15 08:12:27 -0400

    Set up a room for all users to join and leave.

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ application.get("/", function(request, response)
 server = require("http").Server(application);
 server.listen(process.env.PORT || 1271);
 
-
 var io = require("socket.io")(server);
 io.on("connection", function(socket)
 {
+	Partyroom.add(socket);
+	console.log(Partyroom.size());
+	
 	socket.on("queue a video", function(value)
 	{
 		console.log("queue a video: " + value);
@@ -23,4 +25,30 @@ io.on("connection", function(socket)
 		//todo: check that value is a yturl.
 		//todo: add to the end of the queue.
 	});
+	
+	socket.on("disconnect", function()
+	{
+		Partyroom.remove(socket);
+		console.log(Partyroom.size());
+	});
 });
+
+var Partyroom = new function()
+{
+	this.add = function(socket)
+	{
+		this._sockets[socket.id] = socket;
+	}
+	
+	this.remove = function(socket)
+	{
+		delete this._sockets[socket.id];
+	}
+	
+	this.size = function(socket)
+	{
+		return Object.keys(this._sockets).length;
+	}
+	
+	this._sockets = {};
+}
